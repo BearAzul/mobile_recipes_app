61017baa249e8c3f1ad05ff0413bde5f42e9cc81
BearAzul <adyaabduazizulhakim@gmail.com>
2026-02-23 14:05:05 +0700

    added cron

---
+++ backend_recipes/src/config/cron.js
@@ -0,0 +1,16 @@
+import cron from "cron"
+import https from "https"
+
+const job = new cron.CronJob("*/14 * * * *", function () { 
+  https.get(process.env.API_URL, (res) => {
+    if(res.statusCode === 200) {
+      console.log("API is up and running")
+    } else {
+      console.error(`API is down. Status code: ${res.statusCode}`)
+    }
+  }).on("error", (err) => {
+    console.error(`Error checking API: ${err.message}`)
+  })
+})
+
+export default job
