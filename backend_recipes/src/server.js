b00159393a1d5e8d4c3dfc11aa5d8c78963ea394
BearAzul <adyaabduazizulhakim@gmail.com>
2026-02-23 14:07:03 +0700

    start the job

--- backend_recipes/src/server.js
+++ backend_recipes/src/server.js
@@ -3,10 +3,14 @@ import { ENV } from "./config/env.js"
 import cors from "cors"
 import { notFound, errorHandler } from "./middlewares/error.middleware.js"
 import favoritesRoute from "./routes/favorites.route.js"
+import job from "./config/cron.js"
 
 const PORT = ENV.PORT || 5001
 
 const app = express()
+
+job.start()
+
 app.use(express.json())
 app.use(cors())
 
